2373e2e44378eab6f7eb6a2669dd46a901e8f56f
Beatriz <beatriz.sabbatini@gmail.com>
2021-11-02 23:02:38 -0300

    main: fixed compile error (removed sass  config)

--- next.config.js
+++ next.config.js
@@ -1,17 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const path = require('path')
-const withSass = require('@zeit/next-sass');
-module.exports = withSass({
-cssModules: true
-})
-module.exports = {
-sassOptions: {
-includePaths: [path.join(__dirname, 'styles')],
-},
-}
-
-
 module.exports = {
   reactStrictMode: true,
 }
