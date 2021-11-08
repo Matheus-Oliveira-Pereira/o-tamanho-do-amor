28ea2ac80ca93c9fcbe7ea24a72742286aa69b9c
Beatriz <beatriz.sabbatini@gmail.com>
2021-11-07 21:34:44 -0300

    fix: correção do texto da sinopse

--- src/components/SynopsisScreen/index.tsx
+++ src/components/SynopsisScreen/index.tsx
@@ -10,8 +10,9 @@ export const Synopsis: React.FC = () => {
         </ImageWrapper>
         <TextsContainer>
           <Title>SINOPSE</Title>
-          <Text>O que seria uma simples excursão de estudantes londrinenses para um museu na cidade de São Paulo 
-            torna-se, na verdade. um emaranhado de diferen- tes acontecimentos. Adolescentes e adultos expõem 
+          <Text>
+            O que seria uma simples excursão de estudantes londrinenses para um museu na cidade de São Paulo 
+            torna-se, na verdade, um emaranhado de diferentes acontecimentos. Adolescentes e adultos expõem 
             particularidades de suas famílias, amizades, paqueras e, claro, surgem discussões. Muitas discussões. 
             Surpresas ocorrem ao longo da viagem e depois, muitas outras também. Mas afinal, o que seria o misterioso 
             Domo Mágico? Como uma criança autista poderia ser o motivo principal de grande revolução tecnológica?
